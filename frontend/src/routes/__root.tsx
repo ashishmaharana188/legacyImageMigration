01cdb1056b3ad57894404d3ca9903b044ee0d312
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2026-01-30 20:11:50 +0530

    ..

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,14 +1,11 @@
-import { createRootRoute } from '@tanstack/react-router';
-import { WebSocketProvider } from '../contexts/WebSocketProvider';
-import { TaskLogProvider } from '../contexts/TaskLogContext';
-import App from '../App';
+import { createRootRoute } from "@tanstack/react-router";
+import { TaskLogProvider } from "../contexts/TaskLogContext";
+import App from "../App";
 
 export const Route = createRootRoute({
   component: () => (
-    <WebSocketProvider>
-      <TaskLogProvider>
-        <App />
-      </TaskLogProvider>
-    </WebSocketProvider>
+    <TaskLogProvider>
+      <App />
+    </TaskLogProvider>
   ),
 });
